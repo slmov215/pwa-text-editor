8af7baf06d0f6f884716aeb0cab0e4055e08c5de
Stephen <127278005+slmov215@users.noreply.github.com>
2023-08-04 00:11:26 -0400

    fixed put and get logic inside database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,36 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// The logic to this method accepts some content and adds it to the database
+export const putDb = async (content) => {
+  
+  const textDB = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const tx = textDB.transaction('jate', 'readwrite');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log(' Data saved to database', result);
+};
+// console.error('putDb not implemented');
+
+// The logic to this method gets the content from the database
+export const getDb = async () => {
+  const textDB = await openDB('jate', 1);
+
+  const tx = textDB.transaction('jate', 'readonly');
+
+  const store = tx.objectStore('jate');
+
+  const request = store.get(1);
+
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
+// console.error('getDb not implemented');
 
 initdb();
